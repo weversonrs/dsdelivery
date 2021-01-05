7fd5c09c6a8578701987d02a0bd2ccec5920f78c
weversonrs <weversonr@gmail.com>
2021-01-04 22:18:00 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/devdelivery/DevdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.devdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevdeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/devdelivery/DevdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.devdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
